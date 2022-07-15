2180c4d07cac2cc518fa0ca874f96a261ca03f52
Иван Нигаев <Nihaiev@it.group>
2022-07-15 22:35:41 +0300

    task 1

--- js/task1.js
+++ js/task1.js
@@ -1,20 +1,20 @@
-function greet(name) {
-  return `Добро пожаловать ${name}.`;
-}
+// function greet(name) {
+//   return `Добро пожаловать ${name}.`;
+// }
 
-// Вызываем функцию greet
-// и выводим результат в консоль
-console.log(greet("Mango"));
-// Добро пожаловать Mango.
+// // Вызываем функцию greet
+// // и выводим результат в консоль
+// console.log(greet("Mango"));
+// // Добро пожаловать Mango.
 
-// Выводим функцию greet
-// в консоль не вызывая её
-console.log(greet);
-/*
-ƒ greet() {
-     return `Добро пожаловать ${name}.`;
-}
-*/
+// // Выводим функцию greet
+// // в консоль не вызывая её
+// console.log(greet);
+// /*
+// ƒ greet() {
+//      return `Добро пожаловать ${name}.`;
+// }
+// */
 
 function makePizza() {
   return "Your pizza is being prepared, please wait.";
