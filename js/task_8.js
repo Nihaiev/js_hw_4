b6c8630ecfef97d96075d302c57763ae0ff0731d
Иван Нигаев <Nihaiev@it.group>
2022-07-16 12:37:37 +0300

    task 8

---
+++ js/task_8.js
@@ -0,0 +1,14 @@
+// Звичайне оголошення функції
+function classicAdd(a, b, c) {
+  return a + b + c;
+}
+
+// Те саме стрілочною функцією
+const arrowAdd = (a, b, c) => {
+  return a + b + c;
+};
+
+calculateTotalPrice = (quantity, pricePerItem) => {
+  // Change code above this line
+  return quantity * pricePerItem;
+}
