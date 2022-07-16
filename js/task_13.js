83e51d46a26f4b4bc3b17784f052248244325f9a
Иван Нигаев <Nihaiev@it.group>
2022-07-16 15:35:38 +0300

    task 13

---
+++ js/task_13.js
@@ -0,0 +1,44 @@
+// const dirtyMultiply = (array, value) => {
+//   for (let i = 0; i < array.length; i += 1) {
+//     array[i] = array[i] * value;
+//   }
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// dirtyMultiply(numbers, 2);
+// // Відбулася мутація вихідних даних - масиву numbers
+// console.log(numbers); // [2, 4, 6, 8, 10]
+
+
+// const pureMultiply = (array, value) => {
+//   const newArray = [];
+
+//   array.forEach(element => {
+//     newArray.push(element * value);
+//   });
+
+//   return newArray;
+// };
+
+// const numbers = [1, 2, 3, 4, 5];
+// const doubledNumbers = pureMultiply(numbers, 2);
+
+// // Мутація вихідних даних не відбулася
+// console.log(numbers); // [1, 2, 3, 4, 5]
+// // Функція повернула новий масив зі зміненими даними
+// console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+
+function changeEven(numbers, value) {
+  // Change code below this line
+  const newArray = [];
+      if (number % 2 === 0) {
+      number += value;
+      newArray.push(number);
+    } else {
+      newArray.push(number);
+    }
+  });
+  return newArray;
+  };
+
+console.log(object);
